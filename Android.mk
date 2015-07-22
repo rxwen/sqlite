425016207f70f250de14796abd22eb9ba34e00f5
rxwen <rx.wen218@gmail.com>
2015-07-22 20:21:47 +0800

    add sqlite3 source code, based on v3081002

---
+++ Android.mk
@@ -0,0 +1,36 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	sqlite3.c
+
+LOCAL_CFLAGS := -O2 -g
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES += libdl
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_MODULE := libsqlite3
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	shell.c
+
+LOCAL_CFLAGS := -O2 -g
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES += libdl libsqlite3
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_MODULE := sqlite
+
+include $(BUILD_EXECUTABLE)
